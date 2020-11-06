26a7e65967f90a4243946717b6662c346a03ebf5
TNDStudios <info@thenakeddeveloper.com>
2020-11-06 22:47:18 +0000

    [main] Initial Commit

---
+++ Configuration/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TNDStudios.Utils.Config
+{
+    public class Taxonomy
+    {
+        public String Id { get; set; } = Guid.NewGuid().ToString();
+        public String Name { get; set; } = String.Empty;
+        Dictionary<String, TaxonomyNode> Nodes { get; set; }
+    }
+
+    public class TaxonomyNode
+    {
+        public String Id { get; set; } = Guid.NewGuid().ToString();
+        public String Path { get; set; } = String.Empty;
+        Dictionary<String, TaxonomyProperty> Properties { get; set; }
+    }
+
+    public class TaxonomyProperty
+    {
+        public String Id { get; set; } = Guid.NewGuid().ToString();
+        public String Name { get; set; } = String.Empty;
+        public String Value { get; set; } = String.Empty;
+    }
+}
