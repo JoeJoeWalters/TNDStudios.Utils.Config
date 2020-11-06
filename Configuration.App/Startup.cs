5444d5b7c5f7bfcb6aa4b9ca86d6c8bdea013657
TNDStudios <info@thenakeddeveloper.com>
2020-11-06 23:10:16 +0000

    [main] Added in mocked up taxonomy for testing

--- Configuration.App/Startup.cs
+++ Configuration.App/Startup.cs
@@ -1,10 +1,103 @@
-﻿using System;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TNDStudios.Utils.Config;
+
+[assembly: FunctionsStartup(typeof(Configuration.App.Startup))]
 
 namespace Configuration.App
 {
-    class Startup
+    public class Startup : FunctionsStartup
     {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<TaxonomyContainer>(
+                new TaxonomyContainer()
+                {
+                    Taxonomies = new Dictionary<string, Taxonomy>()
+                    {
+                        {
+                            "main",
+                            new Taxonomy()
+                            {
+                                Id = Guid.NewGuid().ToString(),
+                                Name = "main",
+                                Nodes = new Dictionary<string, TaxonomyNode>()
+                                {
+                                    { 
+                                        "Tenant1.Application1", 
+                                        new TaxonomyNode()
+                                        {
+                                            Properties = new Dictionary<string, TaxonomyProperty>()
+                                            {
+                                                {
+                                                    "API::Unchanging",
+                                                    new TaxonomyProperty()
+                                                    {
+                                                        Type = typeof(String),
+                                                        Value = "Some Random Value"
+                                                    }
+                                                },
+                                                {
+                                                    "API::Version", 
+                                                    new TaxonomyProperty() 
+                                                    { 
+                                                        Type = typeof(float),
+                                                        Value = "3.1"
+                                                    } 
+                                                }
+                                            }
+                                        } 
+                                    },
+                                    {
+                                        "Tenant1.Application1.Module1",
+                                        new TaxonomyNode()
+                                        {
+                                            Properties = new Dictionary<string, TaxonomyProperty>()
+                                            {
+                                                {
+                                                    "API::Version",
+                                                    new TaxonomyProperty()
+                                                    {
+                                                        Type = typeof(float),
+                                                        Value = "3.2"
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    },
+                                    {
+                                        "Tenant1.Application1.Module2",
+                                        new TaxonomyNode()
+                                        {
+                                            Properties = new Dictionary<string, TaxonomyProperty>()
+                                            {
+                                                {
+                                                    "API::Version",
+                                                    new TaxonomyProperty()
+                                                    {
+                                                        Type = typeof(float),
+                                                        Value = "3.3"
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+
+            /*
+            builder.Services.AddSingleton<IMyService>((s) => {
+                return new MyService();
+            });
+
+            builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+            */
+        }
     }
 }
--- Configuration/Class1.cs
+++ Configuration/Class1.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 
 namespace TNDStudios.Utils.Config
 {
+    public class TaxonomyContainer
+    {
+        public Dictionary<String, Taxonomy> Taxonomies { get; set; }
+    }
+
     public class Taxonomy
     {
         public String Id { get; set; } = Guid.NewGuid().ToString();
         public String Name { get; set; } = String.Empty;
-        Dictionary<String, TaxonomyNode> Nodes { get; set; }
+        public Dictionary<String, TaxonomyNode> Nodes { get; set; }
     }
 
     public class TaxonomyNode
     {
-        public String Id { get; set; } = Guid.NewGuid().ToString();
-        public String Path { get; set; } = String.Empty;
-        Dictionary<String, TaxonomyProperty> Properties { get; set; }
+        public Dictionary<String, TaxonomyProperty> Properties { get; set; }
     }
 
     public class TaxonomyProperty
     {
-        public String Id { get; set; } = Guid.NewGuid().ToString();
-        public String Name { get; set; } = String.Empty;
+        public Type Type { get; set; } = typeof(String);
         public String Value { get; set; } = String.Empty;
     }
 }
